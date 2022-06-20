429538ed72820fc1c7607f206bbc74037b729c67
Himanshi <himanshibobde@gmail.com>
2022-06-20 18:27:23 -0500

    second commit

--- Test.java
+++ Test.java
@@ -3,5 +3,7 @@ class Test{
     public static void main(String[] args){
 
         System.out.println("Hello World! Welcome To JAVA..");
+        int a=10, b=10;
+        System.out.print(a+b);
     }
 }
