76cf3ece16430ac605c12b1be6b11090e7dd231a
chandrakant <asterisctemp@gmail.com>
2022-06-21 05:50:57 -0700

    work done i add sub feat

--- Test.java
+++ Test.java
@@ -1,9 +1,11 @@
-class Test{
-    
-    public static void main(String[] args){
+class Test {
+
+    public static void main(String[] args) {
 
         System.out.println("Hello World! Welcome To JAVA..");
-        int a=10, b=10;
-        System.out.print(a+b);
+        int a = 10, b = 10;
+        System.out.print(a + b);
+
+        System.out.println("sub " + (a - b));
     }
 }
